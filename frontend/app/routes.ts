ee44ad98a3c16e04649fa53d82ebb3d835d17f37
Rohan Shanavas <34649755+rohanshanavas@users.noreply.github.com>
2026-07-09 21:24:22 +0100

    Added files for login and registering users

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/root/home.tsx")] satisfies RouteConfig;
+export default [
+    layout("routes/auth/auth-layout.tsx", [
+        index("routes/root/home.tsx"),
+        route("sign-in", "routes/auth/sign-in.tsx"),
+        route("sign-up", "routes/auth/sign-up.tsx"),
+        route("forgot-password", "routes/auth/forgot-password.tsx"),
+        route("verify-email", "routes/auth/verify-email.tsx"),
+        route("reset-password", "routes/auth/reset-password.tsx"),])
+] satisfies RouteConfig;
--- frontend/app/routes/root/home.tsx
+++ frontend/app/routes/root/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Route } from '../../+types/root';
 import { Button } from '@/components/ui/button';
+import { Link } from 'react-router';
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -11,8 +12,13 @@ export function meta({ }: Route.MetaArgs) {
 
 const Homepage = () => {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="w-full h-screen flex items-center justify-center gap-4">
+      <Link to="/sign-in">
+        <Button className="bg-blue-500 text-white">Login</Button>
+      </Link>
+      <Link to="/sign-up">
+        <Button variant="outline" className="bg-blue-500 text-white">Sign Up</Button>
+      </Link>
     </div>
   )
 }
