ed82a3c9d0625451b744d7ddabd47cff10b365b8
Rohan Shanavas <34649755+rohanshanavas@users.noreply.github.com>
2026-07-14 17:20:43 +0100

    Integrate React Query provider across the application

---
+++ frontend/app/provider/authContext.tsx
@@ -0,0 +1,55 @@
+import type { User } from "@/types";
+import { createContext, useContext, useState } from "react";
+
+
+interface AuthContextType {
+    user: User | null;
+    isAuthenticated: boolean;
+    isLoading: boolean;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const login = async (email: string, password: string) => {
+
+        console.log("Logging in with email:", email, "and password:", password);
+
+    };
+
+    const logout = async () => {
+
+        console.log("Logging out");
+
+    };
+
+    const values = {
+        user,
+        isAuthenticated,
+        isLoading,
+        login,
+        logout
+    };
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}</AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+
+    return context;
+
+}
--- frontend/app/routes/auth/auth-layout.tsx
+++ frontend/app/routes/auth/auth-layout.tsx
@@ -1,7 +1,19 @@
+import { useAuth } from '@/provider/authContext';
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Navigate, Outlet } from 'react-router'
 
 const AuthLayout = () => {
+
+    const {isAuthenticated, isLoading} = useAuth();
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isAuthenticated) {
+        return <Navigate to="/dashboard" />;
+    }
+
     return (
         <Outlet />
     )
